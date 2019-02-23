bbfee0275de85a0bfd258d55813e4b0786a132a3
Lucas Kalleu <41480662+LucasKalleu@users.noreply.github.com>
2019-02-23 16:54:42 -0300

    Inserindo Tela de Login

--- index.php
+++ index.php
@@ -1,10 +1,31 @@
 <!DOCTYPE html>
 <html lang="pt-br">
   <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
+    <meta charset="UTF-8">
+    <!--Import Master.css-->
+    <link type="text/css" rel="stylesheet" href="css/master.css">
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
 
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  </head>
+
+  <body>
+      <div class="z-depth-4">
+        <h4>Bom dia Sr...</h4>
+        <form class="" action="" method="POST">
+          <div class="input-field" style="width: 250px; margin-top: 30px;">
+            <input type="password" id="senha" name="senha" class="validate">
+            <label for="senha">Insira sua senha</label>
+            <input type="button" name="ok" value="Entrar" class="btn">
+          </div>
+        </form>
+        </div>
+    <!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
   </body>
 </html>
