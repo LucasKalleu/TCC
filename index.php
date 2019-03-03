7c14b502c12b76d7935a90c6ae5d894ba54f3e45
Lucas Kalleu <41480662+LucasKalleu@users.noreply.github.com>
2019-03-03 19:42:22 -0300

    New UX

--- index.php
+++ index.php
@@ -6,24 +6,28 @@
     <link type="text/css" rel="stylesheet" href="css/master.css">
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!--Import materialize.css-->
-    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   </head>
 
   <body>
-      <div class="z-depth-4">
-        <h4>Bom dia Sr...</h4>
-        <form class="" action="" method="POST">
-          <div class="input-field" style="width: 250px; margin-top: 30px;">
-            <input type="password" id="senha" name="senha" class="validate">
-            <label for="senha">Insira sua senha</label>
-            <input type="button" name="ok" value="Entrar" class="btn">
-          </div>
-        </form>
+
+    <div class="aaa">
+      <h1>LOGIN</h1>
+      <form class="" action="" method="post">
+        <div class="tbox">
+          <input type="text" placeholder="usuario" name="" value="">
         </div>
+
+        <div class="tbox">
+          <input type="password" placeholder="senha" name="" value="">
+        </div>
+
+        <input class="botao" type="submit" name="" value="Entrar">
+      </form>
+      <a href="#" class="b1">ESQUECEU SUA SENHA ?</a>
+    </div>
     <!--Import jQuery before materialize.js-->
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script type="text/javascript" src="js/materialize.min.js"></script>
